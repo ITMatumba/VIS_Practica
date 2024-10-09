23d927b31be270e35f3003cd6cd8ed003dc7a6af
ITMatumba <115963869+ITMatumba@users.noreply.github.com>
2024-10-10 01:12:06 +0700

    Add files via upload

---
+++ Practica_VIS/Practica_VIS/Practica_VIS/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите 1-ое число: ");
+        double numOne = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите 2-ое число: ");
+        double numTwo = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Выберите операцию:\n 1)Сложение(+)\n 2)Вычитание(-)\n 3)Умножение(*)\n 4)Деление(/)");
+        string operation = Console.ReadLine();
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case "+":
+                {
+                    result = plusNum(numOne, numTwo);
+                    break;
+                }
+
+            case "-":
+                {
+                    result = minusNum(numOne, numTwo);
+                    break;
+                }
+
+            case "*":
+                {
+                    result = multiplicationNum(numOne, numTwo);
+                    break;
+                }
+
+            case "/":
+                {
+                    result = delNum(numOne, numTwo);
+                    break;
+                }
+        }
+
+        Console.WriteLine("Результат вычислений: " + $"{result}");
+    }
+
+
+    public static double plusNum(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    public static double minusNum(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    public static double multiplicationNum(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+
+    public static double delNum(double num1, double num2)
+    {
+        return num1 / num2;
+    }
+
+}
